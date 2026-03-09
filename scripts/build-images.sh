74524a4611a9a36cdd59c34f8410ed050f39a7af
sasi19S <janirinku4@email.com>
2026-03-09 13:47:54 +0530

    Refactor project structure: move services into services/ folder and add infra + scripts

---
+++ scripts/build-images.sh
@@ -0,0 +1,5 @@
+docker build -t auth-service ../services/auth-service
+docker build -t order-service ../services/order-service
+docker build -t inventory-service ../services/inventory-service
+docker build -t payment-service ../services/payment-service
+docker build -t api-gateway ../services/api-gateway
---
+++ scripts/deploy-k8s.sh
@@ -0,0 +1,1 @@
+kubectl apply -R -f infra/k8s
